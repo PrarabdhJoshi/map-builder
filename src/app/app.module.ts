a652675948dddda7d760c3173a6673c4e1dadffb
PrarabdhJoshi <prarabdh@ufl.edu>
2017-10-09 10:16:51 -0400

    Added Map and Venue Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { VenueComponent } from './venue/venue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    VenueComponent
   ],
   imports: [
     BrowserModule
