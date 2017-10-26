371e960c6355fb7edb3a22629cb2c5b1d3365aa6
PrarabdhJoshi <prarabdh@ufl.edu>
2017-10-26 11:55:52 -0400

    basic_front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { VenueComponent } from './venue/venue.component';
-
+import {HttpClientModule} from '@angular/common/http';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,MatRadioModule,MatToolbarModule, MatCardModule,MatInputModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +17,20 @@ import { VenueComponent } from './venue/venue.component';
     VenueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAsf3dSFPmh3ZuPGxTBQPnAHrliHIjuodk'
+    }),
+    AgmSnazzyInfoWindowModule,
+    BrowserAnimationsModule,MatButtonModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  lat: number = 51.678418;
+  long: number = 7.809007;
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
