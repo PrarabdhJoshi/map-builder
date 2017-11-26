6616d2b38f4850c25e7589dcf713e8e8e46ef1bd
PrarabdhJoshi <prarabdh@ufl.edu>
2017-11-26 14:29:41 -0500

    Dependency Injected for Onboarding Editing

--- src/app/edit-venue/edit-venue.component.ts
+++ src/app/edit-venue/edit-venue.component.ts
@@ -1,7 +1,7 @@
 import { Component,OnInit,ViewChild } from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-edit-venue',
   templateUrl: './edit-venue.component.html',
@@ -13,13 +13,18 @@ export class EditVenueComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   venue_id:any;
+  venue_map:any;
 
-  constructor(private _formBuilder: FormBuilder,private activatedRoute: ActivatedRoute) { }
+  constructor(private _formBuilder: FormBuilder,private activatedRoute: ActivatedRoute, private http:HttpClient) { }
 
   ngOnInit() {
     this.activatedRoute.queryParams.forEach((params: Params) => {
       this.venue_id=this.activatedRoute.snapshot.queryParams['venue_id'];
-      
+  });
+  this.http.get('http://127.0.0.1:5000/api/venue_id/'+this.venue_id).subscribe(data => {
+    // Read the result field from the JSON response.
+    this.venue_map = data[0];
+     console.log(this.venue_map);
   });
 
   console.log(this.venue_id);
