516a5297c2104d7ed22aac9d70ff4a8e6bbec28c
PrarabdhJoshi <prarabdh@ufl.edu>
2017-11-26 22:12:38 -0500

    Service Error for Onboarding Fixed - Fixes #33

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CenterComponent} from './core/main/center.component';
 import { MapComponent } from './map/map.component';
+import {EditVenueComponent} from './edit-venue/edit-venue.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: CenterComponent},
-  {path: 'Service/Sign_in',component: MapComponent}
+  {path: 'Service/Sign_in',component: MapComponent},
+  {path: 'edit', component:EditVenueComponent}
 ];
 
 @NgModule({
--- src/app/edit-venue/edit-venue.component.ts
+++ src/app/edit-venue/edit-venue.component.ts
@@ -2,8 +2,12 @@ import { Component,OnInit,ViewChild } from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import {Http} from '@angular/http';
+import {editService} from './edit-venue.service';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-edit-venue',
+  providers:[editService],
   templateUrl: './edit-venue.component.html',
   styleUrls: ['./edit-venue.component.css']
 })
@@ -13,26 +17,47 @@ export class EditVenueComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   venue_id:any;
-  venue_map:any;
+  venue_map;
+  
 
-  constructor(private _formBuilder: FormBuilder,private activatedRoute: ActivatedRoute, private http:HttpClient) { }
+  constructor(private activatedRoute: ActivatedRoute,private http:Http,private _editService:editService) { 
+    
+  }
 
-  ngOnInit() {
+  ngOnInit():void {
     this.activatedRoute.queryParams.forEach((params: Params) => {
       this.venue_id=this.activatedRoute.snapshot.queryParams['venue_id'];
   });
-  this.http.get('http://127.0.0.1:5000/api/venue_id/'+this.venue_id).subscribe(data => {
-    // Read the result field from the JSON response.
-    this.venue_map = data[0];
-     console.log(this.venue_map);
-  });
-
   console.log(this.venue_id);
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-  }
+   this._editService.searchvenuebyid(this.venue_id).subscribe(result => {
+    if(result.status === 400){return;}
+    else{
+      this.venue_map = result[0];
+    }
+  });
+  
+   
+  // .subscribe(result1 => {
+  //   if(result1.status === 400){return;}
+  //   else{
+  //     this.venue_map = result1;
+  //   }
+  // });
+  
+  // this.http.get('http://127.0.0.1:5000/api/venue_id/'+this.venue_id).subscribe(data => {
+  //   // Read the result field from the JSON response.
+  //   this.venue_map = data[0];
+  //    //console.log(this.venue_map);
+  // });
+
+  //console.log(this.venue_id);
+//   //   this.firstFormGroup = this._formBuilder.group({
+//   //     firstCtrl: ['', Validators.required]
+//   //   });
+//   //   this.secondFormGroup = this._formBuilder.group({
+//   //     secondCtrl: ['', Validators.required]
+//   //   });
+//   // }
+
+}
 }
