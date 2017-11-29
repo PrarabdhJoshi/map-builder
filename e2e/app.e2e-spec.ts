c97a9a97fea66722469f612fdfd82ad25749a16d
PrarabdhJoshi <prarabdh@ufl.edu>
2017-11-29 10:14:18 -0500

    Mapped all venues to their Unique Logo and photo - Fixes #46

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('map-builder-ui App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app');
   });
 });
