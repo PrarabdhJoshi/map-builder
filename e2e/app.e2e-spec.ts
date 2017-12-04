b93bcce520b8016b4b7ee6dd064fa71766df4c88
BharathAlva <kbharathalva@gmail.com>
2017-12-04 17:34:53 -0500

    e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { AppPage } from './app.po';
 import {browser, by, element} from 'protractor';
 
-
 describe('map-builder-ui App', () => {
   let page: AppPage;
 
@@ -16,9 +15,17 @@ describe('map-builder-ui App', () => {
     const usersignupconfirmPassword = element(by.id('signupconfirmPassword')).sendKeys('testuser123');
     const signupbutton = element(by.id('signupButton'));
     signupbutton.click();
-
     expect(element.all(by.tagName('tr')).count()).toBe(0);
   });
+
+  it('Title Button Redirection before login', () => {
+    browser.get('/login');
+    const titlebutton = element(by.id('beforelogin'));
+    titlebutton.click();
+    const titlebutton1  = element(by.id('beforelogin'));
+    titlebutton1.click();
+  });
+
 
   it('Login Page Failure test', () => {
     browser.get('/login');
@@ -26,7 +33,6 @@ describe('map-builder-ui App', () => {
     const userPassword = element(by.id('loginPassword')).sendKeys('testuser123');
     const button = element(by.id('loginButton'));
     button.click();
-
     expect(element.all(by.tagName('tr')).count()).toBe(0);
   });
 
@@ -41,9 +47,7 @@ describe('map-builder-ui App', () => {
     expect(element.all(by.tagName('tr')).count()).toBe(0);
   });
 
-
-
-  it('Home Page test', () => {
+  it('Home Page radioButton Test', () => {
     browser.get('/login');
     const userEmail = element(by.id('loginEmail')).sendKeys('test@gmail.com');
     const userPassword = element(by.id('loginPassword')).sendKeys('testuser123');
@@ -54,31 +58,60 @@ describe('map-builder-ui App', () => {
     radiobuttonLocation.click();
     const radiobuttonName = element(by.id('nameButton'));
     radiobuttonName.click();
-    const venue = element(by.id('venueInput')).sendKeys('Macys Restaurant');
-    
-    expect(element.all(by.tagName('tr')).count()).toBe(0);
   });
 
-/*
-  it('Map Page test', () => {
-    browser.get('/map');
+
+  it('Home Page Search Bar Test', () => {
+    browser.get('/login');
     const userEmail = element(by.id('loginEmail')).sendKeys('test@gmail.com');
     const userPassword = element(by.id('loginPassword')).sendKeys('testuser123');
     const button = element(by.id('loginButton'));
     button.click();
 
-    const radiobuttonLocation = element(by.id('locationButton'));
-    radiobuttonLocation.click();
     const radiobuttonName = element(by.id('nameButton'));
     radiobuttonName.click();
     const venue = element(by.id('venueInput')).sendKeys('Macys Restaurant');
     
-
     expect(element.all(by.tagName('tr')).count()).toBe(0);
   });
-  */
 
 
+ 
+  it('Title Button Redirection After login', () => {
+    browser.get('/login');
+    const userEmail = element(by.id('loginEmail')).sendKeys('test@gmail.com');
+    const userPassword = element(by.id('loginPassword')).sendKeys('testuser123');
+    const button = element(by.id('loginButton'));
+    button.click();
+
+    const titlebutton = element(by.id('afterlogin'));
+    titlebutton.click();
+    const titlebutton1  = element(by.id('afterlogin'));
+    titlebutton1.click();
+  });
+
+  it('Map Page test', () => {
+    browser.get('/login');
+    const userEmail = element(by.id('loginEmail')).sendKeys('test@gmail.com');
+    const userPassword = element(by.id('loginPassword')).sendKeys('testuser123');
+    const button = element(by.id('loginButton'));
+    button.click();
+
+    
+   
+  });
+  
+  it('Logout from Homepage Test', () => {
+    browser.get('/login');
+    const userEmail = element(by.id('loginEmail')).sendKeys('test@gmail.com');
+    const userPassword = element(by.id('loginPassword')).sendKeys('testuser123');
+    const button = element(by.id('loginButton'));
+    button.click();
+
+    const titlebutton = element(by.id('logoutbutton'));
+    titlebutton.click();
+  });
+  
 
 
 });
